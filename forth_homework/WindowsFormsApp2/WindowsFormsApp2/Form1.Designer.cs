6f65bbc451ed4df676e4f8ce760d9e6cb34d6e8f
pgmillow <1310336507@qq.com>
2024-10-27 01:12:16 +0800

    第四次作业

---
+++ forth_homework/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1137, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(75, 28);
+            this.menuToolStripMenuItem.Text = "menu";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 32);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(386, 621);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(386, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(751, 621);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 653);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 可以在这里添加需要处理的代码
+        }
+    }
+}
+
---
+++ forth_homework/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrives();
+        }
+
+        private void LoadDrives()
+        {
+            TreeNode rootNode = new TreeNode("C:");
+            rootNode.Tag = "C:\\"; // 设置根节点的 Tag
+            treeView1.Nodes.Add(rootNode);
+            LoadDirectories("C:\\", rootNode);
+        }
+
+        private void LoadDirectories(string path, TreeNode node)
+        {
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    TreeNode newNode = new TreeNode(Path.GetFileName(dir))
+                    {
+                        Tag = dir // 将路径存储在节点的 Tag 属性中
+                    };
+                    node.Nodes.Add(newNode);
+                    newNode.Nodes.Add(new TreeNode()); // 添加一个虚拟节点用于懒加载
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // 忽略无法访问的目录
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("加载目录时发生错误: " + ex.Message);
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+
+            // 检查节点是否已经加载了子目录
+            if (node.Nodes.Count == 1 && node.Nodes[0].Text == "" && node.Nodes[0].Tag == null)
+            {
+                node.Nodes.Clear();
+                LoadDirectories(node.Tag.ToString(), node);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string selectedPath = GetFullPath(e.Node);
+            LoadFiles(selectedPath);
+        }
+
+        private string GetFullPath(TreeNode node)
+        {
+            string path = node.Tag.ToString();
+            while (node.Parent != null)
+            {
+                node = node.Parent;
+                path = Path.Combine(node.Tag.ToString(), path);
+            }
+            return path;
+        }
+
+        private void LoadFiles(string path)
+        {
+            listView1.Items.Clear();
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    listView1.Items.Add(new ListViewItem(Path.GetFileName(dir)) { Tag = dir });
+                }
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    listView1.Items.Add(new ListViewItem(Path.GetFileName(file)) { Tag = file });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("加载文件时发生错误: " + ex.Message);
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedFilePath = listView1.SelectedItems[0].Tag.ToString();
+                if (File.Exists(selectedFilePath))
+                {
+                    string extension = Path.GetExtension(selectedFilePath).ToLower();
+                    try
+                    {
+                        if (extension == ".exe")
+                        {
+                            Process.Start(selectedFilePath);
+                        }
+                        else if (extension == ".txt")
+                        {
+                            Process.Start("notepad.exe", selectedFilePath);
+                        }
+                        else
+                        {
+                            // 对于其他文件类型，可以添加相应的处理
+                            Process.Start(selectedFilePath);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("打开文件时发生错误: " + ex.Message);
+                    }
+                }
+                else if (Directory.Exists(selectedFilePath))
+                {
+                    // 如果双击的是文件夹，加载该文件夹的内容
+                    LoadFiles(selectedFilePath);
+                }
+            }
+        }
+    }
+}
